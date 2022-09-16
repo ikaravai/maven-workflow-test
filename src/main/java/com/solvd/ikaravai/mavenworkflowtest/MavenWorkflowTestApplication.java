2c5fc9bb5e8121e648451ff5cb18c998f6c1ce79
Ivan Karavai <ikaravai@solvd.com>
2022-09-16 15:28:49 +0300

    first commit

---
+++ src/main/java/com/solvd/ikaravai/mavenworkflowtest/MavenWorkflowTestApplication.java
@@ -0,0 +1,13 @@
+package com.solvd.ikaravai.mavenworkflowtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenWorkflowTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenWorkflowTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solvd/ikaravai/mavenworkflowtest/MavenWorkflowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvd.ikaravai.mavenworkflowtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenWorkflowTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
